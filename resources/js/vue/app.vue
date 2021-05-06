e3bb3e01e5a1a48dceac980292279965531a103f
Abdur Razzaque-H <mdarh411@gmail.com>
2021-05-06 22:12:45 +0600

    first commit

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <AddItemForm />
+        </div>
+        <ListView />
+    </div>
+</template>
+
+<script>
+import AddItemForm from "./AddItemForm"
+import ListView from "./ListView"
+export default {
+    components: {
+        AddItemForm,
+        ListView
+    }
+}
+</script>
+
+<style scoped>
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+
+.heading{
+    background: #e6e6e6;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+</style>>
+
